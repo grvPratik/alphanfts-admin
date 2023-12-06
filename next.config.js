9ac2f9e9be230403916bd932e3d0c35222897f4e
pratikgrv <pratikgaurav37@gmail.com>
2023-12-06 21:47:32 +0530

    header check

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
       headers: [
         { key: "Access-Control-Allow-Credentials", value: "true" },
         { key: "Access-Control-Allow-Origin", value: "https://www.alphanfts.app/" },
-        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS," },
+        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Cache-Control" }, // Add Cache-Control here
       ]
     }
