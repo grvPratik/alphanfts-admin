4365e794bacb9dc8e59ed858ced332bb1e82d91d
pratikgrv <pratikgaurav37@gmail.com>
2024-01-08 22:38:39 +0530

    middleware correction

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/register","/collections", "/collections/:path*"] };
+export const config = { matcher: ["/register","/projects", "/projects/:path*"] };
